f2587cbc731261b7c17b18d3d1dc88a1adc832bf
Binita Wosti <wostibinita@gmail.com>
2026-04-10 01:33:23 -0400

    Add files via upload

---
+++ docstring_mode_script.py
@@ -0,0 +1,64 @@
+"""
+hello.py
+Purpose: This program is for practicing variables and list
+Author: Binita
+Date: April 7, 2026
+"""
+#Activity 1- Variable
+#Make variable for DNA sequence
+DNAsequence= 'ATGCCTA'
+
+#Print the varible
+print(DNAsequence)
+
+#Make variable for a number
+Number= 6
+
+#print the variable
+print(Number)
+
+#Activity 2- Make list
+#List days in a week
+weekdays = ['Mo', 'Tu', 'We', 'Th', 'Fr'] 
+
+#Print 4th weekday (count starts from zero)
+print(weekdays[3])
+
+#Make an empty list
+empty_list = [ ]
+
+#Print empty list
+print(empty_list)
+
+#Add a new element to weekdays
+weekdays.append('Sa')
+
+#Print weekdays list after one day addition
+print(weekdays)
+
+#Remove an element from the weekdays
+weekdays.remove('Mo')
+
+#Print weekdays after one day removal
+print(weekdays)
+
+#Activity- 3
+#Create a list of colors
+color = ['blue', 'white', 'pink']
+
+#Print the color list
+print(color[1])
+
+#Add a new color in the color list
+color.append('yellow')
+
+#Remove a color from the color list
+color.remove('blue')
+
+#Print the final list
+print(color)
+
+
+
+
+
