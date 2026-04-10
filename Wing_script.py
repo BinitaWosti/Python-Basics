8972ee88518ceaf87cdc249b305dc2269e2414e8
Binita Wosti <wostibinita@gmail.com>
2026-04-10 01:13:47 -0400

    Add files via upload

---
+++ Wing_script.py
@@ -0,0 +1,6 @@
+#Both double quote ("") and single quote ('') works in python
+print("hello!")
+print('hello!')
+print(2+2)
+print("Life is beautiful")
+print("life", "is", "beautiful!", sep = "-", end = "\n")
